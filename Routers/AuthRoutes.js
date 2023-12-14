e3b68a57154db216b4d5d6fdba67c88ae8fb3d73
Jarvis9960 <ankitfukte11@gmail.com>
2023-12-14 15:06:14 +0530

    improving passport js local stragery

--- Routers/AuthRoutes.js
+++ Routers/AuthRoutes.js
@@ -1,11 +1,40 @@
 import express from "express";
 const router = express.Router();
-import { loginController, signUp } from "../Controllers/AuthController.js";
+import { signUp } from "../Controllers/AuthController.js";
 import { logout } from "../Controllers/GoogleAuthController.js";
 import { protectedRoute } from "../Middlewares/protectedRoute.js";
+import passport from "passport";
 
 router.post("/signup", signUp);
-router.post("/login", loginController);
+
+// local stragery
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      // Authentication failed
+      return res.status(401).json({ status: false, message: info.message });
+    }
+
+    // Authentication succeeded
+    req.logIn(user, (loginErr) => {
+      if (loginErr) {
+        return next(loginErr);
+      }
+
+      console.log(req.isAuthenticated());
+      return res.status(201).json({
+        status: true,
+        message: "Login Successfully",
+        user: req.user,
+      });
+    });
+  })(req, res, next);
+});
+
 router.post("/logout", protectedRoute, logout);
 
 export default router;
