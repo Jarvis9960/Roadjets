94011b364166ec0289629a1f1c46f359a0a8b66d
Ankit Fukte <100926362+Jarvis9960@users.noreply.github.com>
2023-12-15 13:26:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       "https://roadjets-jarvis9960.vercel.app",
+      "https://www.roadjets.in"
     ],
     credentials: true,
   })
